c18ed28094669afc3fc674cb1fd50186949085c8
jimmyfargo <zblizzard@live.com>
2016-03-21 08:14:40 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Title</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"css/main.sass\"></head><body><h1>Hello Liliette</h1><h4>It's nice to see you again</h4></body></html>");;return buf.join("");
+}
